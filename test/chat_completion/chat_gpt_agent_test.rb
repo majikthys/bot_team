aaf365068e9cd2e753a2ec3308a2f36d22b4902b
Mike <mike+github@technomonkey.net>
2024-01-01 17:07:45 -0800

    Spec-ify specs and reorg for guard
    Make the test file structure match lib and make it all runnable by guard

--- test/chat_completion/chat_gpt_agent_test.rb
+++ test/chat_completion/chat_gpt_agent_test.rb
@@ -1,17 +1,16 @@
-require 'minitest/autorun'
-require 'chat_completion/chat_gpt_agent'
+# frozen_string_literal: true
+
+require 'test_helper'
 
 describe ChatGptAgent do
   $LOAD_PATH << 'test/modules' unless $LOAD_PATH.include?('test/modules')
 
-  def setup
-    @subject = ChatGptAgent.new
-  end
+  subject { ChatGptAgent.new }
 
   it 'talks to api' do
-    @subject.chat_gpt_request.functions=nil
-    @subject.chat_gpt_request.add_user_message('please say hello')
-    result = @subject.call
+    subject.chat_gpt_request.functions=nil
+    subject.chat_gpt_request.add_user_message('please say hello')
+    result = subject.call
 
     assert_equal ChatGptResponse, result.class
     assert result.message.downcase.include?('hello'), "result should include 'hello'"
---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/focus'
+
+require_relative '../lib/chat_completion'
+
+require 'modules/leaf'
+require 'modules/switchboard'
