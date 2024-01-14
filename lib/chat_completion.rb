4b471db4368ac747089a90c7f1735c303bcd11e8
Mike <mike+github@technomonkey.net>
2024-01-13 20:11:18 -0800

    Having pry loaded is handy

--- lib/chat_completion.rb
+++ lib/chat_completion.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 require_relative 'chat_completion/agent_runner'
 require_relative 'chat_completion/chat_gpt_agent'
 require_relative 'chat_completion/chat_gpt_request'
